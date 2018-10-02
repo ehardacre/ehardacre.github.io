89a9af13d2a91770c40010db1fccf37972fa6de5
ehardacre <hardacre.ethan@gmail.com>
2018-10-02 10:28:57 +0200

    Content pages created

--- js/main.js
+++ js/main.js
@@ -196,3 +196,24 @@
   }
 
 })(jQuery);
+
+//modal CONTENT
+var modal = document.getElementById('myModal');
+var btns = document.getElementsByClassName("portfolioButton");
+var span = document.getElementsByClassName("close")[0];
+
+for(i = 0; i < btns.length; i++){
+  btns[i].onclick = function() {
+    modal.style.display = "block";
+  }
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event){
+  if (event.target == modal) {
+    modal.style.display = "none"
+  }
+}
