96fb50bc6e8c6ed2507d3d844cccab3fb912fbde
ehardacre <hardacre.ethan@gmail.com>
2018-10-03 12:52:05 +0200

    Work uploaded

--- js/main.js
+++ js/main.js
@@ -199,13 +199,18 @@
 
 //modal CONTENT
 var modal = document.getElementById('myModal');
-var btns = document.getElementsByClassName("portfolioButton");
+var title = document.getElementById('modalTitle');
 var span = document.getElementsByClassName("close")[0];
+var type = "default"
+var slideIndex = 1;
+var slides = [];
 
-for(i = 0; i < btns.length; i++){
-  btns[i].onclick = function() {
-    modal.style.display = "block";
-  }
+function showModal(name){
+  modal.style.display = "block";
+  title.innerText = name;
+  type = name;
+  slideIndex = 1;
+  showSlides(slideIndex)
 }
 
 span.onclick = function() {
@@ -215,5 +220,41 @@ span.onclick = function() {
 window.onclick = function(event){
   if (event.target == modal) {
     modal.style.display = "none"
+  }
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  slides = document.getElementsByClassName(type);
+  all = document.getElementsByClassName("slide");
+  var j;
+  for(j = 0; j < all.length; j++){
+    if(!all[j].classList.contains(type)){
+      all[j].style.display = "none";
+    }else{
+      all[j].style.display = "block";
+    }
+  }
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+function hideSlides(){
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
 }
